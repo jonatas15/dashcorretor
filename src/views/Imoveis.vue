5d3cef558f9d08522a8374ae5ce0dfcafd54fa4d
jonatas15 <jonatas.webdeveloper@gmail.com>
2023-03-29 16:22:20 -0300

    Lista e paginação de imóveis vindos da API

--- src/views/Imoveis.vue
+++ src/views/Imoveis.vue
@@ -18,35 +18,51 @@
             <sub>Em construção</sub>
             <hr>
             <div class="table-responsive-md">
-              <table class="table table-hover">
+              <table class="table table-hover" id="my-table">
                   <thead>
                       <tr>
-                      <th scope="col">Imóvel</th>
+                      <th scope="col">Imóvel no Site</th>
                       <th scope="col">Url - Campanha</th>
-                      <th scope="col">Acessos/Mês</th>
+                      <th scope="col">Acessos/Fevereiro</th>
                       <th scope="col">Visitas feitas</th>
                       </tr>
                   </thead>
                   <tbody>
-                      <tr>
-                      <th scope="row">PIN - 1235</th>
-                      <td>https://cafeimobiliaria.com.br/campanha</td>
-                      <td>243</td>
-                      <td>3</td>
+                      <tr v-for="imv in imoveis">
+                        <th scope="row">
+                          <a :href="'https://cafeimobiliaria.com.br/imovel/' + imv.codigo" target="_blank">
+                            PIN - {{ imv.codigo }}
+                          </a>
+                        </th>
+                        <td>Acesso normal</td>
+                        <td>{{imv.acessos}}</td>
+                        <td>(In Dev.)</td>
                       </tr>
-                      <tr>
-                      <th scope="row">PIN - 8734</th>
-                      <td>https://cafeimobiliaria.com.br</td>
-                      <td>454</td>
-                      <td>7</td>
-                      </tr>
-                      <!-- <tr>
-                      <th scope="row">3</th>
-                      <td colspan="2">Larry the Bird</td>
-                      <td>@twitter</td>
-                      </tr> -->
                   </tbody>
               </table>
+              <!-- <b-pagination
+                v-model="pagina"
+                :total-rows="rows"
+                :per-page="20"
+                aria-controls="my-table"
+            ></b-pagination> -->
+            <ul class="pagination">
+              <li class="page-item">
+                <a v-if="pagina > 1" class="page-link" @click="paginarmenos()" aria-label="Previous">
+                  <span aria-hidden="true">&laquo;</span>
+                  <span class="sr-only">Previous</span>
+                </a>
+              </li>
+              <li v-for="page in paginas" class="page-item" :class="page == pagina ? ' active' : ''">
+                <a class="page-link" @click="paginar(page)">{{page}}</a>
+              </li>
+              <li class="page-item">
+                <a v-if="pagina < totalpage" class="page-link" @click="paginarmais()" aria-label="Next">
+                  <span aria-hidden="true">&raquo;</span>
+                  <span class="sr-only">Next</span>
+                </a>
+              </li>
+            </ul>
             </div>    
         </div>
       </div>
@@ -65,19 +81,65 @@
           corretor: [],
           posicao: 0,
           // urlmarca: "https://cafeimobiliaria.com.br",
-          // urlmarca: "http://localhost",
-          urlmarca: "",
+          urlmarca: "http://localhost:8080",
+          // urlmarca: "",
+          imoveis: [],
+          todosimoveis: [],
+          pagina: 1,
+          totalpage: 1,
+          por_pagina: 25,
+          paginas: [],
+          corretor_id: 12
         }
       },
       components: {
           Progress
       },
+      
+      methods: {
+        paginar(paginaescolhida) {
+          this.pagina = paginaescolhida;
+          var initpage = (this.pagina - 1) * this.por_pagina
+          var fimdpage = initpage + this.por_pagina
+          this.imoveis = this.todosimoveis.filter(d => d.corretor_id == this.corretor_id).slice(initpage, fimdpage);
+        },
+        paginarmais() {
+          if (this.pagina > 0 && this.pagina < this.totalpage) {
+            this.pagina = this.pagina + 1;
+            var initpage = (this.pagina - 1) * this.por_pagina
+            var fimdpage = initpage + this.por_pagina
+            this.imoveis = this.todosimoveis.filter(d => d.corretor_id == this.corretor_id).slice(initpage, fimdpage);
+          }
+        },
+        paginarmenos() {
+          if (this.pagina > 0) {
+            this.pagina = this.pagina - 1;
+            var initpage = (this.pagina - 1) * this.por_pagina
+            var fimdpage = initpage + this.por_pagina
+            this.imoveis = this.todosimoveis.filter(d => d.corretor_id == this.corretor_id).slice(initpage, fimdpage);
+            
+          }
+        }
+      },
       created() {
-        axios.get(this.urlmarca + "../jsons/cafeimoveiscorretor.json").then((res) => {
+        axios.get(this.urlmarca + "/api/imovel").then((res) => {
           console.log(res.data)
+          this.todosimoveis = res.data.filter(d => d.corretor_id == this.corretor_id);
+          this.totalpage = Math.ceil(Number(this.todosimoveis.length)/this.por_pagina);
+          this.imoveis = res.data.filter(d => d.corretor_id == this.corretor_id).slice(0, this.por_pagina);
+          for (let index = 0; index < this.totalpage; index++) {
+            list.push(Number(index + 1)); 
+          }
+          this.paginas = list;
         }).catch((error) => {
           console.log(error);
         });
-      }
+        var list=[];
+      } 
     }
   </script>
+<style>
+  .page-link:hover {
+    cursor: pointer !important;
+  }
+</style>
